c86130fc24d946ad0cda8791044247d4f187affc
Neha Karanjkar <notpublic.notpublic.org>
2017-12-19 14:32:11 +0530

    Updated compilation script

--- 3_Sitar_models/0_Model/compile_Single_server_system.sh
+++ 3_Sitar_models/0_Model/compile_Single_server_system.sh
@@ -1,4 +1,7 @@
-sitar compile --no-logging  -m ./cpp/Simulate_Single_server_system.cpp  -d ./sitar_descriptions/Output/ -d ./include -o SingleServer 
+cd ./sitar_descriptions
+./translate_all.sh
+cd -
+sitar compile --no-logging  -m ./cpp/Simulate_Single_server_system.cpp  -d ./sitar_descriptions/Output/ -d ./include --cflags '-fPIC -DGENERATE_SHARED_LIBRARY_ONLY -std=c++0x' -o SingleServer 
 mv libSingleServer.so ./lib
 mv SingleServer ./exe
 
