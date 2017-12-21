15057378e77d57007bd1f3f52d1de653de5f41fc
Neha Karanjkar <notpublic@notpublic.com>
2017-12-21 12:54:54 +0530

    Optimization script

--- 3_Sitar_models/2_Optimization_case_study/4_Optimize_COBYLA/Optimize_COBYLA.py
+++ 3_Sitar_models/2_Optimization_case_study/4_Optimize_COBYLA/Optimize_COBYLA.py
@@ -10,8 +10,6 @@ import OptimizationProblem
 
 #Create an instance of the optimization problem
 OP = OptimizationProblem.OptimizationProblem()
-OP.rand_seed=42 #rand seed used for each simulation
-
 
 #Fixed parameters (defined in OptimizationProblem.py):
 # OP.p  #arrival probability
@@ -21,22 +19,58 @@ OP.rand_seed=42 #rand seed used for each simulation
 # OP.W  #weight assigned to cost during optimization
 
 
-#create some randomly chosen initial guesses:
-NUM_TRIALS=100
-initial_guesses = []
-
 import random
-random.seed(1)
-
-
-for i in range(NUM_TRIALS):
-    initial_guesses.append([])
-    for j in range(OP.NUM_DIMENSIONS):
-        initial_guesses[i].append(random.randint(OP.bounds[j][0],OP.bounds[j][1]))
-#print "Initial guesses: ",initial_guesses
-
-import numpy
+import time
+import numpy as np
 from scipy.optimize import fmin_cobyla
+
+
+#remove duplicate entries from a list
+def remove_duplicates(list_x):
+    x = list_x[:]
+    for i in range(len(x)):
+        if(i>=len(x)):
+            break
+        for j in range(len(x)):
+            if(j>=len(x)):
+                break
+            if(i!=j and x[i]==x[j]):
+                del x[j]
+    return x
+                
+#generate an array of initial guesses 
+#and store it in a file:
+def generate_initial_guesses(num_guesses, dimensions,bounds):
+
+    random.seed(1)
+    initial_guesses=[]
+    while (len(initial_guesses)<num_guesses):
+        #add a row
+        initial_guesses.append([])
+        #populate it
+        for j in range(dimensions):
+            initial_guesses[-1].append(random.randint(bounds[j][0],bounds[j][1]))
+        #remove duplicates
+        initial_guesses = remove_duplicates(initial_guesses)
+
+    print "Initial guesses: (",len(initial_guesses),")",initial_guesses
+    #store the array to a file:
+    initial_guesses_file = open("Initial_guesses.py", "w")
+    print >>initial_guesses_file, "initial_guesses=",
+    print >>initial_guesses_file, initial_guesses
+    initial_guesses_file.close()
+
+
+
+#Run the following only once to generate the initial guesses:
+#generate_initial_guesses(num_guesses=100, dimensions=OP.NUM_DIMENSIONS, bounds=OP.bounds)
+
+
+#read initial guesses into an array
+import Initial_guesses
+initial_guesses = Initial_guesses.initial_guesses
+NUM_TRIALS=len(initial_guesses)
+
 
 #Python list to store the results
 RESULTS=[]
@@ -45,21 +79,34 @@ Result = namedtuple('Result', 'x_opt f_opt throughput_opt cost_opt fevals time')
 
 
 #Lengths of Simulation runs
-simulation_lengths = [10000,100000]
+simulation_budget = [1000, 10000, 100000, 1000000]
+sim_length_best = max(simulation_budget)
 
 
-import time
-import numpy as np
+def Run_COBYLA(embedding_method):
 
-
-def Run_COBYLA():
+    #select the method used for 
+    #embedding the discrete parameter space
+    #into a continuous one.
+    #
+    #Eg: randomization_embedding,
+    #   simplex_interpolation
+    #   rounding
+    OP.embedding_method=embedding_method
     
-    for l in range(len(simulation_lengths)) :
-        print "Simulation length = ",simulation_lengths[l]
-        OP.sim_length = simulation_lengths[l]
+    for l in range(len(simulation_budget)) :
+        
+        OP.sim_budget = simulation_budget[l]
+        simulation_length = embedding_method.get_simulation_length(simulation_budget[l])
+        print "Simulation budget per point = ",simulation_budget[l]," simulation length = ",simulation_length
         RESULTS.append([])
 
+
         for trial in range(NUM_TRIALS):
+            
+            #set simulation_length for this optimization:
+            OP.sim_length = simulation_length
+
             print "\tOptimization Run = ",trial,
             x_start = initial_guesses[trial]
 
@@ -73,12 +120,15 @@ def Run_COBYLA():
 				x0=x_start,
 				cons=OP.constraints,
                                 iprint=0,
-				rhobeg=2,
+				rhobeg=5,
 				rhoend=1e-3)
             t_end=time.time()
 
             #round x_opt to the nearest decimal point
             x_opt = OP.Round(x_opt)
+            
+            #compute results, using the best possible simulation_length:
+            OP.sim_length = sim_length_best
 
             fevals = OP.objective_function_count 
             f_opt = OP.ObjectiveFunction(x_opt)
@@ -93,17 +143,37 @@ def Run_COBYLA():
 
             RESULTS[l].append(R)
     
-    print " ========================================="
-    print " Printing results to file RESULTS.py ..."
+    print " =============================================================="
     #write the Result array out to a file
-    results_file = open("RESULTS.py", "w")
+    fname = "RESULTS_COBYLA_"+embedding_method.name+".py"
+    print " Printing results to file",fname,"..."
+    results_file = open(fname, "w")
     print >>results_file, "from collections import namedtuple"
     print >>results_file, "Result = namedtuple('Result', 'x_opt f_opt throughput_opt cost_opt fevals time')"
     print >>results_file, "RESULTS=",RESULTS
     print " Done."
-    print " ========================================="
+    print " ==============================================================="
+    results_file.close()
 
-#Run_COBYLA()
 
+def Summarize_results(result_filename):
+    imp = __import__(result_filename)
+    R = imp.RESULTS
+    for i in range(len(R)):
+        print ""
+        print "Sim budget =",simulation_budget[i],"simulation length = ", OP.embedding_method.get_simulation_length(simulation_budget[i]),
+        
+        fopt = [ R[i][j].f_opt for j in range(len(R[i]))]
+        fevals = [ R[i][j].f_opt for j in range(len(R[i]))]
+        times = [ R[i][j].f_opt for j in range(len(R[i]))]
+        
+        print "fopt best=",min(fopt),
+        print "fopt worst=",max(fopt),
+        print "fopt avg=",np.mean(fopt),
+        print "fopt stdev=",np.std(fopt, ddof=1 if len(fopt)>1 else 0),
+
+embedding_method = OptimizationProblem.Embedding_randomization()
+Run_COBYLA(embedding_method)
+Summarize_results("RESULTS_COBYLA_"+embedding_method.name)
 
 
